5d80cb27d2ba54f3e8fc1befa77deb0021ac1909
RussellCall <russellcall@protonmail.com>
2022-01-21 11:14:38 -0600

    1/21 1115 commit

--- src/scripts/field.js
+++ src/scripts/field.js
@@ -1,6 +1,25 @@
-growingPlants = {}
 
-export const addPlant = () => {
-    const seed = {}
-    return seed
+
+//Define a variable with initial value being an empty array
+    //The array will store plants growing in the field.
+
+const fieldOfPlants = []
+
+
+//Define and export a function named addPlant.
+//addPlant function must accept a seed object as input.
+    //Function will add the seed to the field.
+export const addPlant = (seedObj) => {
+    seed = {}
 }
+
+//Define and export a function named usePlants.
+    //Returns a copy of the array of plants.
+
+
+
+    /*---.Be aware that when planting corn, an array of 
+    objects will be provided instead of a single object 
+    like all the other type of seeds. Investigate 
+    the "Array.isArray" method to check if corn got 
+    passed in, or one of the others.---*/
