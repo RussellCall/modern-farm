e37c9c0e46c6f1aca2e1030a6ca4c20bcec1d122
RussellCall <russellcall@protonmail.com>
2022-01-21 14:58:15 -0600

    1/21 1500 commit

---
+++ src/scripts/tractor.js
@@ -0,0 +1,20 @@
+
+
+//Define and export a plantSeeds function.
+    //Function must accept the year's planting plan as input.
+    //(plantingPlan is a parameter).
+export const plantSeeds = (plan) => {
+    
+
+};
+
+plantingPlan = 
+
+
+/* Example growing plan. Actual plan is bigger.
+[
+    ["Potato", "Soybean", "Soybean", "Corn"],
+    ["Wheat", "Corn", "Wheat", "Asparagus"],
+    ["Asparagus", "Wheat", "Soybean", "Corn"],
+    ["Asparagus", "Soybean", "Potato", "Wheat"]
+]  */
