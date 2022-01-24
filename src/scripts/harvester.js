4ef00a9fb8e38b76a82116dfdab24af58d548d25
RussellCall <russellcall@protonmail.com>
2022-01-24 15:23:47 -0600

    1/24 1525 Commit

--- src/scripts/harvester.js
+++ src/scripts/harvester.js
@@ -2,10 +2,27 @@
 //harvestPlants must accept "plantsArray" as input.
 //harvestPlants must return seedObj.
 
-export const harvestPlants = (plantsArray) {
-    let seedObjects = [],
+export const harvestPlants = (plantsArray) => {
+    let seedObjects = []
+
+    //Iterate the array of growing plants.
     for (const plant of plantsArray) {
+
+        //On each plant, get the value of the output property.
+        if (plant.type === "corn") {
+
+            const cropOutput = plant.output / 2
+            for (let i = 0; i< corn.output; i++) {
+
+                seedObjects.push(plant)
+            }
+        } else {
+
+            for (let i = 0; i< plant.output; i++) {
+
+                seedObjects.push(plant)
+            }
+        }
         
-    }
-    return seedObjects
+    }return seedObjects
 };
