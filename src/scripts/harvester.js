e7e1e48bfabf6caa992f59ac7d5ab00b684407a2
RussellCall <russellcall@protonmail.com>
2022-01-24 14:20:24 -0600

    1/24 1420 Commit

---
+++ src/scripts/harvester.js
@@ -0,0 +1,11 @@
+//define and export harvestPlants function.
+//harvestPlants must accept "plantsArray" as input.
+//harvestPlants must return seedObj.
+
+export const harvestPlants = (plantsArray) {
+    let seedObjects = [],
+    for (const plant of plantsArray) {
+        
+    }
+    return seedObjects
+};
