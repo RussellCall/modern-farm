3db27e0c4ab3553ece945100fab3ada4c5ee7b1b
RussellCall <russellcall@protonmail.com>
2022-01-24 10:41:24 -0600

    1/24 1040 Commit

--- src/scripts/tractor.js
+++ src/scripts/tractor.js
@@ -1,3 +1,22 @@
+import {createAsparagus} from "./seeds/asparagus.js";
+const asparagusSeed = createAsparagus();
+
+import {createCorn} from "./seeds/corn.js";
+const cornSeed = createCorn();
+
+import {createPotato} from "./seeds/potato.js";
+const potatoSeed = createPotato();
+
+import {createSoybean} from "./seeds/soybean.js";
+const soybeanSeed = createSoybean();
+
+import {createSunflower} from "./seeds/sunflower.js";
+const sunflowerSeed = createSunflower();
+
+import {createWheat} from "./seeds/wheat.js";
+const wheatSeed = createWheat();
+
+
 
 
 //Define and export a plantSeeds function.
@@ -6,7 +25,30 @@
 export const plantSeeds = (newCreatePlan) => {
     
 
-};
+}
+for (const plan of plantingPlan) {
+    for (const plantType of plan) {
+        if (plantType === "asparagus") {
+            const asparagusSeed = createAsparagus()
+        }
+        if (plantType === "corn") {
+            const cornSeed = createCorn()
+        }
+        if (plantType === "potato") {
+            const potatoSeed = createPotato()
+        }
+        if (plantType === "soybean") {
+            const soybeanSeed = createSoybean()
+        }
+        if (plantType === "sunflower") {
+            const sunflowerSeed = createSunflower()
+        }
+        if (plantType === "wheat") {
+            const wheatSeed = createWheat()
+        }
+    }
+}
+
 
 
 /* Example growing plan. Actual plan is bigger.
