3f1cfeb726ea3c38b0082061d8c1307a3b0e9678
RussellCall <russellcall@protonmail.com>
2022-01-24 16:01:20 -0600

    end of day, must wrap up html template

---
+++ src/scripts/catalog.js
@@ -0,0 +1,9 @@
+//Define and export a "Catalog" function.
+export const Catalog = (harvestedFoodArray) => {
+
+    //Catalog function should iterate the array of food objects.
+    for (plant of harvestedFoodArray) {
+
+        //html tamplate...
+    }
+}
