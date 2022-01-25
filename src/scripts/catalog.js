e5fd9b346d68247aed7f135cb51c735ba45ec134
RussellCall <russellcall@protonmail.com>
2022-01-25 11:44:33 -0600

    1/25 1145 comiit

--- src/scripts/catalog.js
+++ src/scripts/catalog.js
@@ -4,6 +4,11 @@ export const Catalog = (harvestedFoodArray) => {
     //Catalog function should iterate the array of food objects.
     for (plant of harvestedFoodArray) {
         //Iterate food array.
+        let element = document.querySelector(".container")
+
         //html template...
+        element.innerHTML += `<section class="plant">${plant.name}</section>`
+        
+
     }
 }
